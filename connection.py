4079af6029377ace0ca446486c88b255e1cf8522
Volalex2000 <Volkov.AD@phistech.edu>
2019-12-03 14:18:33 +0300

    Добавлен модуль для общения клиента и сервера

---
+++ connection.py
@@ -0,0 +1,26 @@
+def read_message(who):  # who - кто сообщение вызывает
+    # принимает значения: 2 (client), 1 (server) 
+    """
+    Отвечает за чтение сообщения
+    """
+    other = return_other(who)
+    file_for_readng = open("{}_.txt".format(other),'r',encoding = 'utf-8')
+    message = file_for_readng.readlines
+    file_for_readng.truncate()
+    return message
+
+
+def write_message(who, message):  # message - сообщение, которое необходимо передать клиенту, who - кто сообщение вызывает
+    # принимает значения: 2 (client), 1 (server) 
+    """
+    Отвечает за создание сообщения
+    """
+    file_for_write = open("{}_.txt".format(who),'a')
+    file_for_write.write(str(message) + '\n')
+    
+    
+def return_other(who):
+    if who == "client":
+        return "server"
+    else:
+        return "client"
