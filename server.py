1a65bf36077e6d5e62599824a7b413a15fadd23d
IstominArseniy <istomin15arseniy@gmail.com>
2019-12-03 14:10:21 +0300

    add mage initialize in server

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import BattleField_class as bf
+import Mage_class as mg
 import connection as con
 FIELD_WIDTH = 10
 FIELD_HEIGHT = 10
 battle_filed = bf.BattleField(FIELD_WIDTH, FIELD_HEIGHT, 0)
 for i in range(FIELD_HEIGHT):
     for j in range(FIELD_WIDTH):
-        if battle_filed.field[i][j].type == "Cell":
-            s = str(i)+' '+str(j) + ' ' + "grey"
+        if battle_filed.field[i][j].type == 'Cell':
+            s = 'draw ' + str(i) + ' ' + str(j) + ' ' + 'grey'
             con.write_messages("server", s)
+mage = mg.Mage(0, 0)
+s = 'draw ' + str(0) + ' ' + str(0) + ' ' + 'red'
+con.write_messages("server", s)
+
 
 
 
