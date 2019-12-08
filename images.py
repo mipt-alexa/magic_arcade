b132017b3bc4c13d1e611d0874a26d14b232831e
phystech-aa <volkova.aa@phystec.edu>
2019-12-08 23:26:39 +0300

    working on

--- images.py
+++ images.py
@@ -3,10 +3,12 @@ from PIL import ImageTk,Image
 
 
 """ 1 = камушки """
-""" 2  = маг в красном """
+""" 2  = стена """
+""" 3 = deep elf demonologist"""
+"""" 4 = deep elf mage"""
 
 
-N = 2
+N = 4
 """число картинок"""
 images = [None]
 
@@ -15,7 +17,7 @@ def load_all_images(canv):
     """загружает все изображения, должна быть вызвана сразу после иницализации app"""
     global images
     for i in range(1, N+1):
-        path = "images_library/" + str(i) + ".jpg"
+        path = "images_library/" + str(i) + ".png"
         images.append(ImageTk.PhotoImage(Image.open(path), canv))
         print('tried to load')     #testing
 
